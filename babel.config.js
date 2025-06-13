6833622c85f0b2222df54bf830bac246f047150f
MinhND <Mr.Lonely905B@gmail.com>
2025-06-14 06:15:22 +0700

    Add module resolver configuration to Babel and TypeScript for improved path aliasing

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@components': './src/components',
+          '@assets': './src/assets',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+          '@hooks': './src/hooks',
+          '@navigation': './src/navigation',
+          '@constants': './src/constants',
+          '@services': './src/services',
+        },
+      },
+    ],
+  ],
 };
